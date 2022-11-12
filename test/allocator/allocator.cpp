c62b4a917876c5f6b1327ef849dc3f90f7a4e1ea
Natnael <the.defalt8@gmail.com>
2022-11-12 20:19:19 +0300

    AddTest: allocator[_nt]

--- test/allocator/allocator.cpp
+++ test/allocator/allocator.cpp
@@ -1,62 +1,63 @@
-#include <type_traits>
 #include <pptest>
 #include <colored_printer>
 #include <ds/allocator>
 
-Test(allocator_test)
+Test(default_allocator_test)
 {
-	Testcase(allocator_test, test_allocate_requirements)
-	{
-		AssertTrue(std::is_same<decltype(ds::Allocator::allocate(size_t(0), size_t(0))), void *>::value);
-	} TestcaseEnd(allocator_test, test_allocate_requirements)
+	TestInit(default_allocator_test);
 
-	Testcase(allocator_test, test_deallocate_requirements)
+	Testcase(test_allocate_requirements)
 	{
-		AssertTrue(std::is_same<decltype(ds::Allocator::deallocate((void *)nullptr)), void>::value);
-	} TestcaseEnd(allocator_test, test_deallocate_requirements)
+		AssertTrue(ds::is_same<decltype(ds::DefaultAllocator::allocate(size_t(0), ds::align_t(0))), void *>::value);
+	} TestcaseEnd(test_allocate_requirements);
 
-	Testcase(allocator_test, test_passing_allocation)
+	Testcase(test_deallocate_requirements)
+	{
+		AssertTrue(ds::is_same<decltype(ds::DefaultAllocator::deallocate((void *)nullptr)), void>::value);
+	} TestcaseEnd(test_deallocate_requirements);
+
+	Testcase(test_passing_allocation)
 	{
 		constexpr size_t size_ = 32;
-		auto block_ = ds::Allocator::allocate(size_);
+		auto block_ = ds::DefaultAllocator::allocate(size_);
 		AssertNotNull(block_);
-		ds::Allocator::deallocate(block_);
-	} TestcaseEnd(allocator_test, test_passing_allocation)
+		ds::DefaultAllocator::deallocate(block_);
+	} TestcaseEnd(test_passing_allocation);
 
-	Testcase(allocator_test, test_failing_allocation)
+	Testcase(test_failing_allocation)
 	{
 		constexpr size_t size_ = -1;
 		void * block_ = nullptr;
-		ExpectThrow(ds::Allocator::allocation_failure const &, block_ = ds::Allocator::allocate(size_));
+		ExpectThrow(ds::DefaultAllocator::allocation_failure const &, block_ = ds::DefaultAllocator::allocate(size_));
 		ExpectNull(block_);
-		ds::Allocator::deallocate(block_);
-		ExpectThrow(std::bad_alloc const &, block_ = ds::Allocator::allocate(size_));
+		ds::DefaultAllocator::deallocate(block_);
+		ExpectThrow(std::bad_alloc const &, block_ = ds::DefaultAllocator::allocate(size_));
 		ExpectNull(block_);
-		ds::Allocator::deallocate(block_);
-	} TestcaseEnd(allocator_test, test_failing_allocation)
+		ds::DefaultAllocator::deallocate(block_);
+	} TestcaseEnd(test_failing_allocation);
 
-	Testcase(allocator_test, test_zero_size_allocation)
+	Testcase(test_zero_size_allocation)
 	{
 		constexpr size_t size_ = 0;
-		auto block_ = ds::AllocatorNT::allocate(size_);
+		auto block_ = ds::DefaultAllocatorNT::allocate(size_);
 		AssertNotNull(block_);
-		ds::AllocatorNT::deallocate(block_);
-	} TestcaseEnd(allocator_test, test_zero_size_allocation)
+		ds::DefaultAllocatorNT::deallocate(block_);
+	} TestcaseEnd(test_zero_size_allocation);
 
-	Registry(allocator_test)
-	{
-		Register(allocator_test, test_allocate_requirements)
-		Register(allocator_test, test_deallocate_requirements)
-		Register(allocator_test, test_passing_allocation)
-		Register(allocator_test, test_failing_allocation)
-		Register(allocator_test, test_zero_size_allocation)
-	};
+};
 
-} TestEnd(allocator_test);
+TestRegistry(default_allocator_test)
+{
+	Register(test_allocate_requirements)
+	Register(test_deallocate_requirements)
+	Register(test_passing_allocation)
+	Register(test_failing_allocation)
+	Register(test_zero_size_allocation)
+};
 
 template <class C> using reporter_t = pptest::colored_printer<C>;
 
 int main()
 {
-	return allocator_test().run_all(reporter_t<allocator_test>(pptest::normal));
+	return default_allocator_test().run_all(reporter_t<default_allocator_test>(pptest::normal));
 }
--- test/allocator/allocator_nt.cpp
+++ test/allocator/allocator_nt.cpp
@@ -1,59 +1,61 @@
-#include <type_traits>
 #include <pptest>
 #include <colored_printer>
 #include <ds/allocator>
 
-Test(allocator_nt_test)
+Test(default_allocator_nt_test)
 {
-	Testcase(allocator_nt_test, test_allocate_requirements)
-	{
-		AssertTrue(std::is_same<decltype(ds::AllocatorNT::allocate(size_t(0), size_t(0))), void *>::value);
-	} TestcaseEnd(allocator_nt_test, test_allocate_requirements);
+	TestInit(default_allocator_nt_test);
 
-	Testcase(allocator_nt_test, test_deallocate_requirements)
+	Testcase(test_allocate_requirements)
 	{
-		AssertTrue(std::is_same<decltype(ds::AllocatorNT::deallocate((void *)nullptr)), void>::value);
-	} TestcaseEnd(allocator_nt_test, test_deallocate_requirements);
+		AssertTrue(ds::is_same<decltype(ds::DefaultAllocatorNT::allocate(size_t(0), ds::align_t(0))), void *>::value);
+	} TestcaseEnd(test_allocate_requirements);
 
-	Testcase(allocator_nt_test, test_passing_allocation)
+	Testcase(test_deallocate_requirements)
+	{
+		AssertTrue(ds::is_same<decltype(ds::DefaultAllocatorNT::deallocate((void *)nullptr)), void>::value);
+	} TestcaseEnd(test_deallocate_requirements);
+
+	Testcase(test_passing_allocation)
 	{
 		constexpr size_t size_ = 32;
-		auto block_ = ds::AllocatorNT::allocate(size_);
+		auto block_ = ds::DefaultAllocatorNT::allocate(size_);
 		AssertNotNull(block_);
-		ds::AllocatorNT::deallocate(block_);
-	} TestcaseEnd(allocator_nt_test, test_passing_allocation);
+		ds::DefaultAllocatorNT::deallocate(block_);
+	} TestcaseEnd(test_passing_allocation);
 
-	Testcase(allocator_nt_test, test_failing_allocation)
+	Testcase(test_failing_allocation)
 	{
 		constexpr size_t size_ = -1;
 		void * block_ = nullptr;
-		AssertThrowNone(block_ = ds::AllocatorNT::allocate(size_));
+		AssertThrowNone(block_ = ds::DefaultAllocatorNT::allocate(size_));
 		ExpectNull(block_);
-		ds::AllocatorNT::deallocate(block_);
-	} TestcaseEnd(allocator_nt_test, test_failing_allocation);
+		ds::DefaultAllocatorNT::deallocate(block_);
+	} TestcaseEnd(test_failing_allocation);
 
-	Testcase(allocator_nt_test, test_zero_size_allocation)
+	Testcase(test_zero_size_allocation)
 	{
 		constexpr size_t size_ = 0;
-		auto block_ = ds::AllocatorNT::allocate(size_);
+		auto block_ = ds::DefaultAllocatorNT::allocate(size_);
 		AssertNotNull(block_);
-		ds::AllocatorNT::deallocate(block_);
-	} TestcaseEnd(allocator_nt_test, test_zero_size_allocation);
+		ds::DefaultAllocatorNT::deallocate(block_);
+	} TestcaseEnd(test_zero_size_allocation);
 
-	Registry(allocator_nt_test)
-	{
-		Register(allocator_nt_test, test_allocate_requirements)
-		Register(allocator_nt_test, test_deallocate_requirements)
-		Register(allocator_nt_test, test_passing_allocation)
-		Register(allocator_nt_test, test_failing_allocation)
-		Register(allocator_nt_test, test_zero_size_allocation)
-	};
+};
 
-} TestEnd(allocator_nt_test);
+
+TestRegistry(default_allocator_nt_test)
+{
+	Register(test_allocate_requirements)
+	Register(test_deallocate_requirements)
+	Register(test_passing_allocation)
+	Register(test_failing_allocation)
+	Register(test_zero_size_allocation)
+};
 
 template <class C> using reporter_t = pptest::colored_printer<C>;
 
 int main()
 {
-	return allocator_nt_test().run_all(reporter_t<allocator_nt_test>(pptest::normal));
+	return default_allocator_nt_test().run_all(reporter_t<default_allocator_nt_test>(pptest::normal));
 }
